6213201b87c97025c24f035aed78ed5ec52b0c7c
rhys <lev.k.kim@gmail.com>
2023-01-01 01:15:01 -0500

    styling dropdown

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -4,14 +4,12 @@ module.exports = {
       if (data.draft) {
         return false;
       }
-
       return data.permalink;
     },
     eleventyExcludeFromCollections(data) {
       if (data.draft || data.permalink === false) {
         return true;
       }
-
       return data.eleventyExcludeFromCollections;
     }
   }
